6963b15d76753c9f380ae222c9235dbcee003430
vince15dk <vince15dk@gmail.com>
2021-11-21 17:27:59 +0900

    update

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 SHELL := /bin/bash
 
 # expvarmon -ports=":4000" -vars="build,requests,goroutines,errors,panics,mem:memstats.Alloc"
+# hey -m GET -c 100 -n 10000  http://localhost:3000/v1/test
 
 # ==============================================================================
 # Building containers
--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,6 +2,8 @@ package testgrp
 
 import (
 	"context"
+	"errors"
+	"github.com/vince15dk/myservice3/business/sys/validate"
 	"github.com/vince15dk/myservice3/foundation/web"
 	"go.uber.org/zap"
 	"math/rand"
@@ -16,9 +18,8 @@ type Handlers struct {
 // Test handler is for development.
 func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
 	if n := rand.Intn(100); n%2 == 0 {
-		//return validate.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
-		//return web.NewShutdownError("restart service")
-		panic("testing panic")
+		return validate.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+
 	}
 
 	status := struct {
