e1b24316e25f4d0642055b4d4e2f135d32d98b70
vince15dk <vince15dk@gmail.com>
2021-12-13 14:51:10 +0900

    update

--- business/data/tests/tests.go
+++ business/data/tests/tests.go
@@ -4,11 +4,16 @@ package tests
 import (
 	"bytes"
 	"context"
+	"crypto/rand"
+	"crypto/rsa"
 	"fmt"
 	"github.com/jmoiron/sqlx"
 	"github.com/vince15dk/myservice3/business/data/schema"
+	"github.com/vince15dk/myservice3/business/data/store/user"
+	"github.com/vince15dk/myservice3/business/sys/auth"
 	"github.com/vince15dk/myservice3/business/sys/database"
 	"github.com/vince15dk/myservice3/foundation/docker"
+	"github.com/vince15dk/myservice3/foundation/keystore"
 	"github.com/vince15dk/myservice3/foundation/logger"
 	"go.uber.org/zap"
 	"io"
@@ -95,6 +100,65 @@ func NewUnit(t *testing.T, dbc DBContainer) (*zap.SugaredLogger, *sqlx.DB, func(
 
 }
 
+// Test owns state for running and shutting down tests.
+type Test struct {
+	DB       *sqlx.DB
+	Log      *zap.SugaredLogger
+	Auth     *auth.Auth
+	Teardown func()
+
+	t *testing.T
+}
+
+// NewIntegration creates a database, seeds it, constructs an authenticator.
+func NewIntegration(t *testing.T, dbc DBContainer) *Test {
+	log, db, teardown := NewUnit(t, dbc)
+
+	// Create RSA keys to enable authentication in our service.
+	keyID := "4754d86b-7a6d-4df5-9c65-224741361492"
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Build an authenticator using this private key and id for the key store.
+	auth, err := auth.New(keyID, keystore.NewMap(map[string]*rsa.PrivateKey{keyID: privateKey}))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	test := Test{
+		DB:       db,
+		Log:      log,
+		Auth:     auth,
+		t:        t,
+		Teardown: teardown,
+	}
+
+	return &test
+
+}
+
+// Token generates an authenticated token for a user.
+func (test *Test) Token(email, pass string) string {
+	test.t.Log("Generating token for test ...")
+
+	store := user.NewStore(test.Log, test.DB)
+	claims, err := store.Authenticate(context.Background(), time.Now(), email, pass)
+	if err != nil {
+		test.t.Fatal(err)
+	}
+
+	token, err := test.Auth.GenerateToken(claims)
+	if err != nil {
+		test.t.Fatal(err)
+	}
+
+	return token
+}
+
+// NewIntegration creates a database, seeds it, constructs an authenticator.
+
 // StringPointer is a helper to get a *string from a string. It is in the tests
 // package because we normally don't want to deal with pointers to basic types
 // but it's useful in some tests.
@@ -108,4 +172,3 @@ func StringPointer(s string) *string {
 func IntPointer(i int) *int {
 	return &i
 }
-
